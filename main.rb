5f9f62e62a3cdc39b66f28821ea4727290a16587
isaac <idgm@pm.me>
2020-03-24 14:43:31 -0600

    bubble_sorted check

--- main.rb
+++ main.rb
@@ -1,4 +1,14 @@
-def bubble_sort
+test = [9,4,3]
 
-
+def bubble_sort(array)
+  (array.length).times do |i|
+    (array.length - 1).times do |i|
+        if array[i] > array[i+1]
+          array[i],array[i+1] = array[i+1],array[i]
+        end
+      end
+  end
+  return array
 end
+
+print bubble_sort(test)
