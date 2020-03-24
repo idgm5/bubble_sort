0579792edbb75443a02383030c67e5b0f297844b
isaac <idgm@pm.me>
2020-03-24 15:58:40 -0600

    fix intendations

--- main.rb
+++ main.rb
@@ -1,26 +1,27 @@
 def bubble_sort(array)
-    (array.length).times do |i|
-        (array.length - 1).times do |i|
-            if array[i] > array[i+1]
-                array[i],array[i+1] = array[i+1],array[i]
-            end
-        end
+  (array.length).times do |i|
+    (array.length - 1).times do |i|
+      if array[i] > array[i+1]
+        array[i],array[i+1] = array[i+1],array[i]
+      end
     end
-    return array
+  end
+  return array
 end
 
 def bubble_sort_by(array)
-    (array.length).times do |i|
-        (array.length - 1).times do |i|
-            if array[i].length > array[i+1].length
-                array[i],array[i+1] = array[i+1],array[i]
-            end
-        end
+  (array.length).times do |i|
+    (array.length - 1).times do |i|
+      if array[i].length > array[i+1].length
+        array[i],array[i+1] = array[i+1],array[i]
+      end
     end
-    return array
+  end
+  return array
 end
 
 string_array_sort = bubble_sort_by(["hi","hello","hey"]) do |left,right|
-                      left.length - right.length
-                    end
+  left.length - right.length
+end
+
 print string_array_sort
