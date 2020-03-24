1fe451beb8fd90eae6a180bd8cd7e7cd9fe76d02
bettercallazamat <bettercallazamat@gmail.com>
2020-03-25 03:38:05 +0600

    Add bubble_sort_by function

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 test = [9,4,3]
+test1 = [ "hello", "hi", "bubble", "bye", "yes", "go"]
 
 def bubble_sort(array)
     (array.length).times do |i|
@@ -12,3 +13,17 @@ def bubble_sort(array)
 end
 
 print bubble_sort(test)
+
+
+def bubble_sort_by(array)
+    (array.length).times do |i|
+        (array.length - 1).times do |i|
+            if array[i].length > array[i+1].length
+                array[i],array[i+1] = array[i+1],array[i]
+            end
+        end
+    end
+    return array
+end
+
+print bubble_sort_by(test1)
