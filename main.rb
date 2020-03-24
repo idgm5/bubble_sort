7069cec23193ce685302dc2da9de6baef0538f71
bettercallazamat <bettercallazamat@gmail.com>
2020-03-25 03:24:00 +0600

    Fix indentation

--- main.rb
+++ main.rb
@@ -1,14 +1,14 @@
 test = [9,4,3]
 
 def bubble_sort(array)
-  (array.length).times do |i|
-    (array.length - 1).times do |i|
-        if array[i] > array[i+1]
-          array[i],array[i+1] = array[i+1],array[i]
+    (array.length).times do |i|
+        (array.length - 1).times do |i|
+            if array[i] > array[i+1]
+                array[i],array[i+1] = array[i+1],array[i]
+            end
         end
-      end
-  end
-  return array
+    end
+    return array
 end
 
 print bubble_sort(test)
