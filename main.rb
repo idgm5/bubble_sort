dd3ad59ea0e2e42fee0ca03b9281a985c877b630
isaac <idgm@pm.me>
2020-03-24 16:05:22 -0600

    working in lint errors

--- main.rb
+++ main.rb
@@ -1,27 +1,28 @@
 def bubble_sort(array)
-  (array.length).times do |i|
+  array.length.times do
     (array.length - 1).times do |i|
-      if array[i] > array[i+1]
-        array[i],array[i+1] = array[i+1],array[i]
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
       end
     end
   end
-  return array
+  array
 end
 
 def bubble_sort_by(array)
-  (array.length).times do |i|
+  array.length.times do
     (array.length - 1).times do |i|
-      if array[i].length > array[i+1].length
-        array[i],array[i+1] = array[i+1],array[i]
+      if array[i].length > array[i + 1].length
+        array[i], array[i + 1] = array[i + 1], array[i]
       end
     end
   end
-  return array
+  array
 end
 
-string_array_sort = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+string_array_sort = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
   left.length - right.length
 end
 
 print string_array_sort
+print bubble_sort([0,5,7,2])
